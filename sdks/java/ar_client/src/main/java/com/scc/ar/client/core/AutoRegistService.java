d7c492d269720f536215587f34c8aaf8fecb5e11
shangchengcai <javascc@126.com>
2018-01-12 18:19:45 +0800

    java sdk
    java sdk

---
+++ sdks/java/ar_client/src/main/java/com/scc/ar/client/core/AutoRegistService.java
@@ -0,0 +1,22 @@
+package com.scc.ar.client.core;
+
+/**
+ * @ClassName: AutoRegistService 
+ * @Description: TODO(这里用一句话描述这个类的作用) 
+ * @author shangchengcai@voole.com
+ * @date 2018年1月12日 下午6:19:00 
+ *  
+ */
+public class AutoRegistService {
+
+	/**
+	 * <p>Title: </p> 
+	 * <p>Description: </p> 
+	 * @author shangchengcai@voole.com
+	 * @date 2018年1月12日 下午6:19:00  
+	 */
+	public AutoRegistService() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ sdks/java/ar_client/src/main/java/com/scc/ar/client/scheduler/SchedulerUtil.java
@@ -0,0 +1,95 @@
+package com.scc.ar.client.scheduler;
+
+import org.quartz.Job;
+import org.quartz.JobBuilder;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.SimpleScheduleBuilder;
+import org.quartz.Trigger;
+import org.quartz.TriggerBuilder;
+import org.quartz.impl.StdSchedulerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * @ClassName: SchedulerUtil
+ * @Description: TODO(这里用一句话描述这个类的作用)
+ * @author shangchengcai@voole.com
+ * @date 2018年1月12日 下午5:39:24
+ * 
+ */
+public class SchedulerUtil {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SchedulerUtil.class);
+	private static final String TRIGGER_NAME = "AR_CLIENT_TRIGGER";
+	private static final String TRIGGER_GROUP = "AR_CLIENT_TRIGGER_GROUP";
+	private static final String JOB_NAME = "AR_CLIENT_JOB";
+	private static final String JOB_GROUP = "AR_CLIENT_JOB_GROUP";
+	private static Scheduler scheduler = null;
+	private static SchedulerUtil schedulerUtil;
+
+	/**
+	 * <p>
+	 * Title:
+	 * </p>
+	 * <p>
+	 * Description:
+	 * </p>
+	 * 
+	 * @author shangchengcai@voole.com
+	 * @date 2018年1月12日 下午6:07:52
+	 */
+	private SchedulerUtil() {
+	}
+
+	static {
+		try {
+			scheduler = StdSchedulerFactory.getDefaultScheduler();
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
+			System.exit(1);
+		}
+	}
+
+	public static synchronized SchedulerUtil build() {
+		if (null == schedulerUtil) {
+			schedulerUtil = new SchedulerUtil();
+		}
+		return schedulerUtil;
+	}
+
+	public <T extends Job> SchedulerUtil addJob(Class<T> jobclass, int interval) {
+		try {
+			Trigger trigger = TriggerBuilder.newTrigger().withIdentity(TRIGGER_NAME, TRIGGER_GROUP).startNow()
+					.withSchedule(
+							SimpleScheduleBuilder.simpleSchedule().withIntervalInSeconds(interval).repeatForever())
+					.build();
+			JobDetail job = JobBuilder.newJob(jobclass).withIdentity(JOB_NAME, JOB_GROUP).build();
+			scheduler.scheduleJob(job, trigger);
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
+		}
+		return this;
+	}
+
+	public void start() {
+		try {
+			scheduler.start();
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
+		}
+	}
+
+	public void close() {
+		try {
+			scheduler.shutdown();
+		} catch (SchedulerException e) {
+			e.printStackTrace();
+			LOGGER.error(e.getMessage(), e);
+		}
+	}
+
+}
